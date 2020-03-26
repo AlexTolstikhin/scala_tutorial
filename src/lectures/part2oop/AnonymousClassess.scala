7ca50254f4e9b073a61202471246a2c96e391168
AlexTolstikhin <alex@ascendify.com>
2020-03-25 22:13:59 -0700

    anonymous classes and traits

---
+++ src/lectures/part2oop/AnonymousClassess.scala
@@ -0,0 +1,20 @@
+package lectures.part2oop
+
+object AnonymousClassess extends App {
+  abstract class Animal {
+    def eat: Unit
+  }
+
+  // Anonymous class ==> we have instanciated AnonymousClasses$$annno$1 class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("HAHAHAHA")
+  }
+
+  class Person(name: String) {
+    def sayHi: Unit = println(s"HI my name is ${this.name}")
+  }
+
+  val jim = new Person("Jim"){
+    override def sayHi: Unit = println(s"Hi my name is Jim")
+  }
+}
